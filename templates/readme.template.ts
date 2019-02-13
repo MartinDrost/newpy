6c4461de3d8d01482184e1a8de102acb61e3b022
Martin Drost <info@martindrost.nl>
2019-02-14 00:39:26 +0100

    Improved readme

--- templates/readme.template.ts
+++ templates/readme.template.ts
@@ -1,22 +1,32 @@
 export const readmeTemplate = `\
-### Start virtual env
+### Create virtual env
+This command creates the virtual environment in the current folder.
 \`\`\`
 virtualenv {envName}
 \`\`\`
 
 ### Install requirements.txt
+This command installs all packages with the versions as they are defined in the requirements.txt.
 \`\`\`
 ./{envName}/Scripts/pip.exe pip install -r requirements.txt
 \`\`\`
 
 ### Install package
+This command installs a new package in the virtual environment.
 \`\`\`
 ./{envName}/Scripts/pip.exe install {packageName}
 \`\`\`
 
 ### Save installed packages to requirements.txt
+This command lists the currently installs packages in the requirements.txt.
 \`\`\`
 ./{envName}/Scripts/pip.exe freeze > requirements.txt
+\`\`\`
+
+### (optional) Activate virtualenv
+Running this command allows you to run python from the virtual environment context. This allows you to omit the path to the virtual environment when running pip or python commands.
+\`\`\`
+./{envName}/Scripts/activate.bat
 \`\`\`
 
 `;
